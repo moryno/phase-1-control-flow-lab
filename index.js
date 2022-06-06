d500c7326d24291dd5ddd3661fc2ab9177496e74
Maurice <mauricenganga41@gmail.com>
2022-06-06 14:00:58 +0300

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
+  // Write your code here!
+  let message;
+  if(someValue < 400){
+    message = "This one is on me!"
+  }
+  else if(someValue > 2000 && someValue <= 2500) {
+    message = 'I will gladly take your thirty bucks.'
+  }
+  else if(someValue > 2500) {
+    message = 'No can do.'
+  }
+  return message
+}
+
+function ternaryCheckCity(city){
+  let message;
+  if(city === "NYC"){
+    message = "Ok, sounds good."
+  }
+  else {
+    message = "No go."
+  }
+  return message;
   // Write your code here!
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+  let message;
+  switch (tip) {
+    case tip = "generous":
+      message = 'Thank you so much.';
+      break;
+    case tip = "not as generous":
+      message = "Thank you.";
+      break;
+    
+    default:
+     message = "Bye.";
+      break;
+  }
+  
+  return message;
 }
